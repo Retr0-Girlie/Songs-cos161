e3b0a90a7b51ea0ca09c147947c753c518a2f35c
Retr0-Girlie <iris.mccain@maine.edu>
2026-03-10 14:34:01 -0400

    New CartItem and StoreManager classes
    I created and set up CartItem and created StoreManager. I will try to get more done later tonight. :)

---
+++ src/smartkart/StoreManager.java
@@ -0,0 +1,12 @@
+package smartkart;
+
+import java.util.ArrayList;
+
+public class StoreManager {
+	public static void main(String[] args) {
+		ArrayList<CartItem> cart;
+		ArrayList<Product> inventory;
+		
+	}
+
+}
